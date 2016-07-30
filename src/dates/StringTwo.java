28645dcc15e83b266ba718ec3d5b5047169f9168
Thomas Quezada <thomas.quezada33@gmail.com>
2016-07-29 17:11:23 -0700

    more money, mo probumz

--- src/dates/StringTwo.java
+++ src/dates/StringTwo.java
@@ -6,6 +6,7 @@ package dates;
 public class StringTwo {
     public static void main(String[] args) {
         System.out.println("Codingbat.com/java/String-2");
+        System.out.println("Test123".substring("Test123".lastIndexOf("t")));
 
 
     }
@@ -27,23 +28,94 @@ public class StringTwo {
         return count/2;
     }
     public static boolean catDog(String str) {
-
+        //Return true if the string "cat" and "dog" appear the same number of times in the given string.
         if(str.length()<3) return true;
-        else if((str.contains("dog")||str.contains("cat"))
-                &&str.length()<6) return false;
-        int [] counter = new int[2];
+        else if((str.contains("dog")||str.contains("cat")) && str.length()<6) return false;
+        int cat = 0, dog =0;
         for(int i =0;i<str.length()-2;i++) {
-            if(str.charAt(i)=='c'&&str.charAt(i+1)=='a'&&str.charAt(i+2)=='t'){
-                counter[0]+=1;
+            if(str.charAt(i)=='c'&&str.charAt(i+1)=='a'&&str.charAt(i+2)=='t') cat+=1;
+            else if(str.charAt(i)=='d'&&str.charAt(i+1)=='o'&&str.charAt(i+2)=='g') dog+=1;
+        }
+        if(cat==dog) return true;
+        else return false;
+    }
+    public static int countCode(String str) {
+        //Return the number of times that the string "code" appears anywhere in the given string,
+        //except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+        if (str.length()<4) return 0;
+        int count = 0;
+        for(int i =0;i<str.length()-3;i++){
+            if(str.charAt(i)=='c'&&str.charAt(i+1)=='o'&&str.charAt(i+3)=='e') count++;
+        }
+        return count;
+    }
+
+    public static boolean endOther(String a, String b) {
+        /* Given two strings, return true if either of the strings appears at the very end of the other string,
+    ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+    Note: str.toLowerCase() returns the lowercase version of a string. */
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        int aLen = a.length(), bLen = b.length();
+        if (a.contains(b)&&a.substring(aLen-bLen,aLen).equals(b)) return true;
+        else if (b.contains(a)&&b.substring(bLen-aLen,bLen).equals(a)) return true;
+        else return false;
+    }
+    public static boolean xyzThere(String str) {
+        //Return true if the given string contains an appearance of "xyz" where the xyz is not directly
+        // preceded by a period (.). So "xxyz" counts but "x.xyz" does not.
+        if(str.length()<3) return false;
+        else if(str.length()==3&&str.contains("xyz")) return true;
+        for(int i =0;i<str.length()-3;i++){
+            if(str.charAt(0)=='x'&&str.charAt(1)=='y'&&str.charAt(2)=='z') return true;
+            else if (str.charAt(i)!='.'&&str.charAt(i+1)=='x'&&str.charAt(i+2)=='y'&&str.charAt(i+3)=='z') return true;
+        }
+        return false;
+    }
+    public static boolean bobThere(String str) {
+        //Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+        if(str.length()<3) return false;
+        for(int i = 0;i<str.length()-2;i++){
+            if(str.charAt(i)=='b'&&str.charAt(i+2)=='b')return true;
+        }
+        return false;
+    }
+    public static boolean xyBalance(String str) {
+        /* A String is xy-balanced if for all the 'x' chars in the string,
+        there exists a 'y' char somewhere later in the string. So "xxy" is
+        balanced, but "xyx" is not. One 'y' can balance multiple 'x's.
+        Return true if the given string is xy-balanced. */
+
+        if(str.contains("x")&&!str.contains("y")) return false;
+        else if(!str.contains("x")) return true;
+        String ysub = str.substring(str.lastIndexOf("y"));
+        return(ysub.contains("x"));
+    }
+    public static String mixString(String a, String b) {
+        String mix = "";
+        if(a.length()>b.length()){
+            for(int i =0;i<b.length();i++){
+                mix = mix+a.charAt(i);
+                mix = mix+b.charAt(i);
             }
-            else if(str.charAt(i)=='d'&&str.charAt(i+1)=='o'&&str.charAt(i+2)=='g'){
-                counter[1]+=1;
+            mix = mix + a.substring(b.length());
+        }
+        else if(b.length()>a.length()){
+            for(int i =0;i<a.length();i++){
+                mix = mix+a.charAt(i);
+                mix = mix+b.charAt(i);
+            }
+            mix = mix + b.substring(a.length());
+        }
+        else{
+            for(int i =0;i<a.length();i++){
+                mix = mix+a.charAt(i);
+                mix = mix+b.charAt(i);
             }
         }
-        if(counter[0]==counter[1]) return true;
-        else return false;
-        //just a test
+        return mix;
     }
+    
 
 
 }
